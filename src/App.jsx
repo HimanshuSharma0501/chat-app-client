637c77bd28d529a6d8891ae7bd9c929abe49d93f
Himanshu Sharma <sharmahimanshu050104@gmail.com>
2024-08-26 18:22:12 +0530

    Login page creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
-
+import Login from "./pages/login/Login";
+import "./index.css";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="p-4 h-screen flex items-center justify-center">
+      <Login />
+    </div>
+  );
 };
 
 export default App;
