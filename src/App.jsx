524c6f76f3ca2d3da74059cf0e34b7b283e3bf63
Himanshu Sharma <sharmahimanshu050104@gmail.com>
2024-08-26 21:09:30 +0530

    Sign up functionality connected to backend

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,18 @@ import "./index.css";
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
 import Home from "./pages/home/Home";
+import { Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+      <Toaster position="bottom-center" />
     </div>
   );
 };
